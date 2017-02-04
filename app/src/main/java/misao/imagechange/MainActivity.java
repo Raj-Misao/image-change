dc49041b6c341ce36d73ee9bba6fdee6ba6ee20d
sonu <errajsharma01@gmail.com>
2017-02-04 16:13:52 +0530

    first

---
+++ app/src/main/java/misao/imagechange/MainActivity.java
@@ -0,0 +1,27 @@
+package misao.imagechange;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import static android.R.drawable.sym_action_call;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView)findViewById(R.id.img);
+    }
+
+    public void change(View v)
+    {
+        imageView.setImageResource(R.drawable.funsmile);
+    }
+}
